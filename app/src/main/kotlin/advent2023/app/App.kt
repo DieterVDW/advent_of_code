0b2dd1e3292e5ff19e030b0cd768c1ea046840e7
Dieter Van de Walle <dietervdw@gmail.com>
2023-12-02 19:17:35 +0100

    Puzzle 2A

--- app/src/main/kotlin/advent2023/app/App.kt
+++ app/src/main/kotlin/advent2023/app/App.kt
@@ -4,68 +4,57 @@
 package advent2023.app
 
 fun main() {
-    var input = "1abc2\n" +
-            "pqr3stu8vwx\n" +
-            "a1b2c3d4e5f\n" +
-            "treb7uchet\n"
-    var ret = calc(input)
-    require(ret == 142) { "Expected 142, got ${ret}" }
-
-    input = "two1nine\n" +
-            "eightwothree\n" +
-            "abcone2threexyz\n" +
-            "xtwone3four\n" +
-            "4nineeightseven2\n" +
-            "zoneight234\n" +
-            "7pqrstsixteen"
-    ret = calc(input)
-    require(ret == 281) { "Expected 281, got ${ret}" }
-
-    ret = calc("eighthree")
-    require(ret == 83) { "Expected 83, got ${ret}" }
+    var input = "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green\n" + "Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue\n" + "Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red\n" + "Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red\n" + "Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green"
+    var ret = calc(input, 12, 13, 14)
+    require(ret == 8) { "Expected 8, got ${ret}" }
 
     input = Thread.currentThread().contextClassLoader.getResourceAsStream("input.txt").readAllBytes().decodeToString()
-    val output = calc(input)
+    val output = calc(input, 12, 13, 14)
     println(output)
 }
 
-private fun calc(input: String): Int {
+data class Try(val red: Int, val green: Int, val blue: Int)
+
+data class Game(val num: Int, val tries: List<Try>)
+
+private fun calc(input: String, numRed: Int, numGreen: Int, numBlue: Int): Int {
     println()
-    val replacements = mapOf(
-            "one" to 1,
-            "two" to 2,
-            "three" to 3,
-            "four" to 4,
-            "five" to 5,
-            "six" to 6,
-            "seven" to 7,
-            "eight" to 8,
-            "nine" to 9,
-            "1" to 1,
-            "2" to 2,
-            "3" to 3,
-            "4" to 4,
-            "5" to 5,
-            "6" to 6,
-            "7" to 7,
-            "8" to 8,
-            "9" to 9
-    )
-    val output = input.lines()
+
+    return input.lines()
             .filter { it.isNotBlank() }
-            .map { line ->
-                val firstDigit = replacements.entries
-                        .filter { entry -> line.indexOf(entry.key) >= 0 }
-                        .sortedBy { entry -> line.indexOf(entry.key) }
-                        .map { entry -> entry.value }.first()
-                val lastDigit = replacements.entries
-                        .filter { entry -> line.reversed().indexOf(entry.key.reversed()) >= 0 }
-                        .sortedBy { entry -> line.reversed().indexOf(entry.key.reversed()) }
-                        .map { entry -> entry.value }.first()
-                val num = firstDigit * 10 + lastDigit
-                println("$line -> $num")
-                num
-            }
+            .map { parseGame(it) }
+            .filter { isGamePossibleWithBalls(it, numRed, numGreen, numBlue) }
+            .map { it.num }
             .sum()
-    return output
+}
+
+fun isGamePossibleWithBalls(game: Game, numRed: Int, numGreen: Int, numBlue: Int): Boolean =
+        game.tries.all {
+            it.red <= numRed
+                    && it.green <= numGreen
+                    && it.blue <= numBlue
+        }
+
+fun parseGame(it: String): Game {
+    val game = it.split(":")[0].split(" ")[1].toInt()
+    val tries = it.split(": ")[1].split(";").map {
+        // Parse balls in one try
+        it.split(",")
+                .map { it.trim() }
+                .map {
+                    // Parse "3 blue"
+                    val num = it.split(" ")[0].toInt()
+                    val color = it.split(" ")[1]
+                    when (color) {
+                        "red" -> Try(num, 0, 0)
+                        "green" -> Try(0, num, 0)
+                        "blue" -> Try(0, 0, num)
+                        else -> throw IllegalStateException("Unknown color ${color}")
+                    }
+                }.reduce { a, b ->
+                    Try(a.red + b.red, a.green + b.green, a.blue + b.blue)
+                }
+
+    }
+    return Game(game, tries)
 }
