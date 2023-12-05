f7f71daac6c3ec271327715b1d33fc7cad2443dd
Dieter Van de Walle <dietervdw@gmail.com>
2023-12-05 23:02:48 +0100

    Puzzle 4A

--- app/src/main/kotlin/advent2023/app/App.kt
+++ app/src/main/kotlin/advent2023/app/App.kt
@@ -4,101 +4,56 @@
 package advent2023.app
 
 fun main() {
-    var input = "467..114..\n" +
-            "...*......\n" +
-            "..35..633.\n" +
-            "......#...\n" +
-            "617*......\n" +
-            ".....+.58.\n" +
-            "..592.....\n" +
-            "......755.\n" +
-            "...\$.*....\n" +
-            ".664.598.."
+    var input = "Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53\n" +
+            "Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19\n" +
+            "Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1\n" +
+            "Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83\n" +
+            "Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36\n" +
+            "Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11"
     var ret = calc(input)
-    require(ret == 467835) { "Expected 467835, got ${ret}" }
+    require(ret == 13) { "Expected 13, got ${ret}" }
 
     input = Thread.currentThread().contextClassLoader.getResourceAsStream("input.txt").readAllBytes().decodeToString()
     val output = calc(input)
     println(output)
 }
 
-open class Coord(
-        val row: Int,
-        val column: Int
+data class Card(
+        var num: Int,
+        var winningNumbers: List<Int>,
+        var ownNumbers: List<Int>
 )
 
-class Symbol(
-        row: Int, column: Int, val symbol: Char
-) : Coord(row, column) {
-    override fun toString(): String {
-        return "$row/$column"
-    }
-}
-
-class Number(
-        row: Int, column: Int,
-        val value: Int
-) : Coord(row, column) {
-    override fun toString(): String {
-        return "$row,$column:$value"
-    }
-}
 
 private fun calc(input: String): Int {
     println()
 
-    val coords = input.lines()
-            .flatMapIndexed { row, line ->
-                parseNumbersAndSymbols(row, line)
+    return input.lines()
+            .filter { it.isNotBlank() }
+            .map {
+                println(it)
+                Card(
+                        it.split(":")[0].split(" +".toRegex())[1].toInt(),
+                        splitNumbers(it.split(":")[1].split("|")[0]),
+                        splitNumbers(it.split(":")[1].split("|")[1]),
+                )
             }
-    val gearSymbols = coords.flatMap {
-        when (it) {
-            is Symbol -> listOf(it)
-            else -> listOf()
-        }
-    }.filter { it.symbol == '*' }
+            .map { it.score() }
+            .sum()
+}
 
-    val numbers = coords.flatMap {
-        when (it) {
-            is Number -> listOf(it)
-            else -> listOf()
-        }
+fun splitNumbers(numString: String): List<Int> =
+        numString.trim().split(" +".toRegex())
+                .filter { it.isNotBlank() }
+                .map { it.toInt() }
+
+fun Card.score(): Int {
+    val num = winningNumbers.intersect(ownNumbers).size
+    val score = when (num) {
+        0 -> 0
+        1 -> 1
+        else -> Math.pow(2.0, num - 1.0).toInt()
     }
-
-    return gearSymbols
-            .map { getAdjecentNumbers(numbers, it) }
-            .map { it.map { n -> n.value } }
-            .filter { it.size == 2 }
-            .sumOf { it[0] * it[1] }
-}
-
-fun getAdjecentNumbers(numbers: List<Number>, symbol: Symbol): List<Number> {
-    val adjNumbers = numbers
-            .filter { it.row in symbol.row - 1..symbol.row + 1 }
-            .filter {
-                it.column in symbol.column - it.value.toString().length..symbol.column + 1
-            }
-    println("$symbol -> $adjNumbers")
-    return adjNumbers
-}
-
-fun parseNumbersAndSymbols(row: Int, line: String): List<Coord> {
-    val fold = line.flatMapIndexed { index, c ->
-        when (c) {
-            '.' -> listOf(Coord(row, index))
-            in '0'..'9' -> listOf(Number(row, index, c - '0'))
-            else -> listOf(Symbol(row, index, c))
-        }
-    }.fold(mutableListOf<Coord>()) { list, coord ->
-        if (coord is Number && list.size > 0 && list.last() is Number) {
-            // Merge numbers
-            val lastCoord = list.removeLast() as Number
-            list.add(Number(lastCoord.row, lastCoord.column, lastCoord.value * 10 + coord.value))
-        } else {
-            list.add(coord)
-        }
-        list
-    }.filter { it is Number || it is Symbol }
-    // println("$line -> $fold")
-    return fold
+    println("$winningNumbers $ownNumbers $num $score")
+    return score
 }
