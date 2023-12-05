b453417a602f8da1de0dec2174ebf6ab574878e7
Dieter Van de Walle <dietervdw@gmail.com>
2023-12-05 21:53:59 +0100

    Puzzle 3A

--- app/src/main/kotlin/advent2023/app/App.kt
+++ app/src/main/kotlin/advent2023/app/App.kt
@@ -4,61 +4,97 @@
 package advent2023.app
 
 fun main() {
-    var input = "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green\n" + "Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue\n" + "Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red\n" + "Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red\n" + "Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green"
+    var input = "467..114..\n" +
+            "...*......\n" +
+            "..35..633.\n" +
+            "......#...\n" +
+            "617*......\n" +
+            ".....+.58.\n" +
+            "..592.....\n" +
+            "......755.\n" +
+            "...\$.*....\n" +
+            ".664.598.."
     var ret = calc(input)
-    require(ret == 2286) { "Expected 2286, got ${ret}" }
+    require(ret == 4361) { "Expected 4361, got ${ret}" }
 
     input = Thread.currentThread().contextClassLoader.getResourceAsStream("input.txt").readAllBytes().decodeToString()
     val output = calc(input)
     println(output)
 }
 
-data class Try(val red: Int, val green: Int, val blue: Int)
+open class Coord(
+        val row: Int,
+        val column: Int
+)
 
-data class Game(val num: Int, val tries: List<Try>)
+class Symbol(
+        row: Int, column: Int
+) : Coord(row, column) {
+    override fun toString(): String {
+        return "$row/$column"
+    }
+}
+
+class Number(
+        row: Int, column: Int,
+        val value: Int
+) : Coord(row, column) {
+    override fun toString(): String {
+        return "$row,$column:$value"
+    }
+}
 
 private fun calc(input: String): Int {
     println()
 
-    return input.lines()
-            .asSequence()
-            .filter { it.isNotBlank() }
-            .map { parseGame(it) }
-            .map { minimumBalls(it) }
-            .map { it.power() }
+    val coords = input.lines()
+            .flatMapIndexed { row, line ->
+                parseNumbersAndSymbols(row, line)
+            }
+    val symbols = coords.flatMap {
+        when (it) {
+            is Symbol -> listOf(it)
+            else -> listOf()
+        }
+    }
+    val numbers = coords.flatMap {
+        when (it) {
+            is Number -> listOf(it)
+            else -> listOf()
+        }
+    }
+
+    return numbers
+            .filter { hasAdjecentSymbol(it, symbols) }
+            .map { it.value }
             .sum()
 }
 
-private fun Try.power(): Int {
-    return red * green * blue
-}
-
-fun minimumBalls(game: Game): Try {
-    return Try(
-            red = game.tries.map { it.red }.max(),
-            green = game.tries.map { it.green }.max(),
-            blue = game.tries.map { it.blue }.max(),
-    )
-}
-
-fun parseGame(it: String): Game {
-    val game = it.split(":")[0].split(" ")[1].toInt()
-    val tries = it.split(": ")[1].split(";").map {
-        // Parse balls in one try
-        it.split(",").map { it.trim() }.map {
-            // Parse "3 blue"
-            val num = it.split(" ")[0].toInt()
-            val color = it.split(" ")[1]
-            when (color) {
-                "red" -> Try(num, 0, 0)
-                "green" -> Try(0, num, 0)
-                "blue" -> Try(0, 0, num)
-                else -> throw IllegalStateException("Unknown color ${color}")
-            }
-        }.reduce { a, b ->
-            Try(a.red + b.red, a.green + b.green, a.blue + b.blue)
-        }
-
+fun hasAdjecentSymbol(number: Number, symbols: List<Symbol>): Boolean {
+    val hasAdjecentSymbol = symbols.any {
+        it.row in number.row - 1..number.row + 1
+                && it.column in number.column - 1..(number.column + number.value.toString().length)
     }
-    return Game(game, tries)
+    return hasAdjecentSymbol
+}
+
+fun parseNumbersAndSymbols(row: Int, line: String): List<Coord> {
+    val fold = line.flatMapIndexed { index, c ->
+        when (c) {
+            '.' -> listOf(Coord(row, index))
+            in '0'..'9' -> listOf(Number(row, index, c - '0'))
+            else -> listOf(Symbol(row, index))
+        }
+    }.fold(mutableListOf<Coord>()) { list, coord ->
+        if (coord is Number && list.size > 0 && list.last() is Number) {
+            // Merge numbers
+            val lastCoord = list.removeLast() as Number
+            list.add(Number(lastCoord.row, lastCoord.column, lastCoord.value * 10 + coord.value))
+        } else {
+            list.add(coord)
+        }
+        list
+    }.filter { it is Number || it is Symbol }
+    // println("$line -> $fold")
+    return fold
 }
