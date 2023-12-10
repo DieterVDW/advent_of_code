fe9fdf50045981974facda24f9687127d061ead8
Dieter Van de Walle <dietervdw@gmail.com>
2023-12-10 14:35:34 +0100

    Puzzle 9 A+B, weirdly easy

---
+++ adventOfCode/src/main/kotlin/day9/b/B.kt
@@ -0,0 +1,30 @@
+package day9.b
+
+fun main() {
+    var input = "0 3 6 9 12 15\n" +
+            "1 3 6 10 15 21\n" +
+            "10 13 16 21 30 45"
+    var ret = calc(input)
+    require(ret == 2) { "Expected 2, got ${ret}" }
+
+    input = Thread.currentThread().contextClassLoader.getResourceAsStream("day9.txt").readAllBytes().decodeToString()
+    val output = calc(input)
+    println(output)
+}
+
+fun calc(input: String): Int {
+    return input.lines()
+            .filter { it.isNotBlank() }
+            .map { it.split(" ").map(String::toInt) }
+            .map { it.reversed() }
+            .sumOf { predictNextNumber(it) }
+}
+
+fun predictNextNumber(series: List<Int>): Int {
+    if (series.all { it == 0 }) {
+        return 0
+    } else {
+        val differences = series.zipWithNext().map { it.second - it.first }
+        return series.last() + predictNextNumber(differences)
+    }
+}
